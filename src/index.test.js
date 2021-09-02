f6a21e64bfdbcc2dd92fd8aea372c7d063311039
“seba-nuo” <sebas.a.nunez@email.com>
2021-09-02 15:41:48 -0400

    add test with other team composition

--- src/index.test.js
+++ src/index.test.js
@@ -52,7 +52,60 @@ test("response should be json", async () => {
 
 test("response should update sueldo_completo", async () => {
   const response = await request(app).post("/api").send(jugadores);
-  expect(response.body.jugadores[3].sueldo_completo).toBe(14250)
+  expect(response.body.jugadores[3].sueldo_completo).toBe(14250);
+});
+
+const nuevosJugadores = {
+  jugadores: [
+    {
+      nombre: "Carlos Perez",
+      nivel: "B",
+      goles: 2,
+      sueldo: 30_000,
+      bono: 20_000,
+      sueldo_completo: null,
+      equipo: "rojo",
+    },
+    {
+      nombre: "EL Cuauh 2.0",
+      nivel: "Cuauh",
+      goles: 35,
+      sueldo: 200_000,
+      bono: 30_000,
+      sueldo_completo: null,
+      equipo: "azul",
+    },
+    {
+      nombre: "Cosme Notfulanito",
+      nivel: "C",
+      goles: 6,
+      sueldo: 10_000,
+      bono: 10_000,
+      sueldo_completo: null,
+      equipo: "azul",
+    },
+    {
+      nombre: "El Rulo 2 (pero sin rulos)",
+      nivel: "A",
+      goles: 2,
+      sueldo: 40_000,
+      bono: 25_000,
+      sueldo_completo: null,
+      equipo: "rojo",
+    },
+  ],
+};
+// goles por equipo:    45
+// goles mínimos:       50
+// total equipo:        45/50 = 0.9
+
+// total goles Rulo 2:  2/5        
+// bono de El Rulo 2:   12_500 * 0.9 + 12_500 * 0.4 = 16_250
+
+
+test("response should work with other team", async () => {
+  const response = await request(app).post("/api").send(nuevosJugadores);
+  expect(response.body.jugadores[3].sueldo_completo).toBe(16_250);
 });
 
 afterAll(() => {
