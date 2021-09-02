7f3a65b2b89c5a2a4d98f1c63bbf6450ea148990
“seba-nuo” <sebas.a.nunez@email.com>
2021-09-02 15:16:17 -0400

    refactorize computeSalary && add comments

--- src/util.js
+++ src/util.js
@@ -1,5 +1,4 @@
 const computeSalary = (jugadoresObj) => {
-
   const niveles = {
     A: 5,
     B: 10,
@@ -9,29 +8,32 @@ const computeSalary = (jugadoresObj) => {
 
   const { jugadores } = jugadoresObj;
 
+  // para sumar todos los goles del equipo
   const sumReducer = (accumulator, current) => accumulator + current;
   const golesAnotadosEquipo = jugadores
     .map((jugador) => jugador.goles)
     .reduce(sumReducer);
 
+  // para sumar el mínimo de goles que debe hacer el equipo según su nivel
   const nivelReducer = (accumulator, current) => niveles[current] + accumulator;
   const golesMinimosEquipo = jugadores
     .map((jugador) => jugador.nivel)
     .reduce(nivelReducer, 0);
 
+  // si cumplen o superan la cantidad mínima de goles no reciben sobre el 100%
+  const cumplimientoGoles = (goles, golesMin) =>
+    goles >= golesMin ? 1 : goles / golesMin;
+
   jugadores.forEach((jugador) => {
     const golesMinJugador = niveles[jugador.nivel];
     const golesJugador = jugador.goles;
+    const mitadBono = jugador.bono / 2; // 50% depende del jugador y 50% del equipo
 
-    // si cumplen o superan la cantidad mínima de goles no reciben sobre el 100%
-    const cumplimientoGoles = (goles, golesMin) =>
-      goles >= golesMin ? 1 : goles / golesMin;
     const bonoPersonal =
-      (jugador.bono / 2) * cumplimientoGoles(golesJugador, golesMinJugador);
+      mitadBono * cumplimientoGoles(golesJugador, golesMinJugador);
 
     const bonoEquipo =
-      (jugador.bono / 2) *
-      cumplimientoGoles(golesAnotadosEquipo, golesMinimosEquipo);
+      mitadBono * cumplimientoGoles(golesAnotadosEquipo, golesMinimosEquipo);
     const sueldo_completo = bonoPersonal + bonoEquipo;
 
     jugador.sueldo_completo = Math.round(sueldo_completo);
