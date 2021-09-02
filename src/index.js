309444fcf488f7928c1eda55b193f27286bcbdb2
“seba-nuo” <sebas.a.nunez@email.com>
2021-09-02 19:35:11 -0400

    update start command

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { computeSalary } = require("./util");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
